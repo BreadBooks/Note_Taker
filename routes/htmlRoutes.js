dfcc7fae00fb81ace33011a8f4e27490ffe8cec0
Brett B <65505937+BreadBooks@users.noreply.github.com>
2020-12-15 23:42:55 -0600

    Change

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require("path");
 const route = require("express").Router();
 
 route.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, "../public/notespage.html"));
 });
 
 route.get("*", (req, res) => {
